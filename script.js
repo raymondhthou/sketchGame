5facf61c045d878cdaa63501207f235153b11fe4
hour88 <hour88@Xubuntu.myguest.virtualbox.org>
2024-01-08 15:36:40 +0000

    issue creating grid cells, issue rectified by not using defaultSize variable for cellCreation and width/height

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ let columns = 16;
 
 
 // Create the width and height of our divs in the container by determining the number of pixels
-const gridSketch = document.querySelector('#gridContainer');
-gridSketch.style.width = `${gridSketch}px`;
-gridSketch.style.height = `${gridSketch}px`;
+const gridSketch = document.querySelector("#gridContainer");
+gridSketch.style.width = `${defaultSize}px`;
+gridSketch.style.height = `${defaultSize}px`;
 
 
 // This function will create the actual divs/cells inside the grid container, using the variables of the default grid size, rows, columns, width and height
 function createCells() {
     for (let i = 0; i < (rows * columns); i++) {
-        const cellCreation = document.createElement('div');
-        cellCreation.style.width = `${(gridSketch / columns) - 2}px`;
-        cellCreation.style.height = `${(gridSketch / rows) - 2}px`;
+        const cellCreation = document.createElement("div");
+        cellCreation.style.width = `${(defaultSize / columns) - 2}px`;
+        cellCreation.style.height = `${(defaultSize / rows) - 2}px`;
         
-        cellCreation.classList.add('cells');
+        cellCreation.classList.add("cells");
         gridSketch.appendChild(cellCreation);
     }
 }
+
+createCells();
