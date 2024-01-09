8be0ecc06b9694f9280fb9c916dda609dd597582
hour88 <hour88@Xubuntu.myguest.virtualbox.org>
2024-01-09 11:40:27 +0000

    function createCells refactored

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 // variables to create our Etch a Sketch grid
 const defaultSize = 500;
-let rows = 16;
-let columns = 16;
+let eachCell = 16;
 
 
 // Create the width and height of our divs in the container by determining the number of pixels
 const gridSketch = document.querySelector("#gridContainer");
-gridSketch.style.width = `${defaultSize}px`;
-gridSketch.style.height = `${defaultSize}px`;
+gridSketch.style.width = gridSketch.style.height = `${defaultSize}px`;
 
 
 // Input range functionality for resizing the grid
@@ -24,12 +22,14 @@ function draw() {
 
 
 // This function will create the actual divs/cells inside the grid container, using the variables of the default grid size, rows, columns, width and height
-function createCells() {
-    for (let i = 0; i < (rows * columns); i++) {
+function createCells(eachCell) {
+    const numberOfCells = (eachCell * eachCell);
+    const widthHeight = `${(gridSketch / eachCell) - 2}px`;
+
+    for (let i = 0; i < numberOfCells; i++) {
         const cellCreation = document.createElement("div");
-        cellCreation.style.width = `${(defaultSize / columns) - 2}px`;
-        cellCreation.style.height = `${(defaultSize / rows) - 2}px`;
-        
+        cellCreation.style.width = cellCreation.style.height = widthHeight;
+
         cellCreation.classList.add("cells");
         gridSketch.appendChild(cellCreation);
 
@@ -47,7 +47,7 @@ function resetGrid() {
 
 
 // Eventlistener added for the functionality of the grid slide tab to reset the grid
-slide.oninput = function() {
+slide.oninput = function () {
     let inputText = `${this.value} x ${this.value} (Grid)`;
     rangeValue.innerHTML = inputText;
     resetGrid();
@@ -55,4 +55,4 @@ slide.oninput = function() {
 }
 
 
-createCells();
+createCells(16);
